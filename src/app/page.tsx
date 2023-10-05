bbb0adb62c9b663af2322d5667a6373d7f8833f1
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-05 10:25:04 +0900

    Introduce vannila-extract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { Hi } from "./root.css";
 
 export default function Home() {
   return (
     <main>
-      <div>Hi!</div>
+      <div className={Hi}>Hi!</div>
     </main>
   )
 }
