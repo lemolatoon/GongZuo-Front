ce274a232ffa11e58b8afe30495f35436bd8313b
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-06 14:54:09 +0900

    chore: add index.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { CookiesProvider } from "react-cookie";
 import { useLoggedInUser } from "@/hooks/useLoggedInUser";
 import { useLogout } from "@/hooks/useSession";
 import { Button } from "@/components/ui/button";
-import { ConnectedGongZuoAction } from "@/components/GongZuoAction/ConnectedGongZuoAction";
-import { ConnectedGyomuRadioButton } from "@/components/GyomuRadioButton/ConnectedGyomuRadioButton";
+import { ConnectedGongZuoAction } from "@/components/GongZuoAction";
+import { ConnectedGyomuRadioButton } from "@/components/GyomuRadioButton";
 
 const Inner = () => {
   const { user } = useLoggedInUser();
@@ -13,10 +13,7 @@ const Inner = () => {
   if (user) {
     return (
       <>
-        <div>Hi!</div>
-        <div className="text-3xl font-bold underline">Here is user</div>
-        <div>{JSON.stringify(user)}</div>
-        <ConnectedGyomuRadioButton kind="horizontal" />
+        <ConnectedGyomuRadioButton kind="vertical" />
         <ConnectedGongZuoAction />
         <Button variant="secondary" onClick={logout}>
           ログアウト
---
+++ src/components/GongZuoAction/index.ts
@@ -0,0 +1,1 @@
+export { ConnectedGongZuoAction } from "./ConnectedGongZuoAction";
