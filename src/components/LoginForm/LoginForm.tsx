d388d5915afe00860998ef532133721b5479ccd0
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-06 11:02:34 +0900

    Revert "Impl ui by vanilla extract"
    This reverts commit 53fc565ee03e7e804b9ae3971b680f62bbbe7856.

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,9 +2,6 @@ import { UsersApi } from "@/apiClient";
 import { Button } from "@/components/ui/Button";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { formStyle, inputStyle, loginMessage, wrapper } from "./loginForm.css";
-import { Margin } from "@/styles/mixin";
-import { Label } from "@/components/ui/Label";
 
 export type Input = Parameters<UsersApi["loginPost"]>[0];
 type Props = {
@@ -14,25 +11,18 @@ type Props = {
 export const LoginForm: React.FC<Props> = ({ onSubmit, form }) => {
   const { register, handleSubmit } = form;
   return (
-    <div className={wrapper}>
-      <div className={loginMessage}>ログインしてください</div>
-      <form onSubmit={handleSubmit(onSubmit)} className={formStyle}>
-        <div>
-          <Label marginRight={Margin.SPACE_08}>username</Label>
-          <input {...register("username")} className={inputStyle} />
-        </div>
-        <div>
-          <Label marginRight={Margin.SPACE_08}>password</Label>
-          <input
-            type="password"
-            {...register("password")}
-            className={inputStyle}
-          />
-        </div>
-        <Button variant="secondary" marginTop={Margin.SPACE_08}>
-          ログイン
-        </Button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>
+          username: <input {...register("username")} />
+        </label>
+      </div>
+      <div>
+        <label>
+          password: <input type="password" {...register("password")} />
+        </label>
+      </div>
+      <Button variant="secondary">ログイン</Button>
+    </form>
   );
 };
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { primary, secondary, tertiary } from "./style.css";
-import { MixinProps, constructStyle } from "@/styles/mixin";
 
 const Variant = {
   Primary: "primary",
@@ -13,8 +12,7 @@ type Variant = (typeof Variant)[keyof typeof Variant];
 type Props = {
   variant: Variant;
   children?: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement> &
-  MixinProps;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 export const Button: React.FC<Props> = ({ variant, children, ...props }) => {
   let className;
   switch (variant) {
@@ -31,5 +29,9 @@ export const Button: React.FC<Props> = ({ variant, children, ...props }) => {
       console.error("invalid variant", variant);
       className = tertiary;
   }
-  return <button className={className}>{children}</button>;
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
 };
