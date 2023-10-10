0df8ed568c050c69016f05182da19ca714f58c42
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-10 10:56:17 +0900

    Add invalidateAllUsers

--- src/hooks/useAllUsers.ts
+++ src/hooks/useAllUsers.ts
@@ -1,6 +1,6 @@
 import { errorHandler } from "@/lib/error";
 import { useUserClient } from "@/state/client";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
 
 type Handler = (msg: string) => void;
@@ -20,4 +20,16 @@ export const useQueryAllUsers = (handler: Handler) => {
   const res = useQuery([getAllUsersKey], fetcher);
 
   return res;
+};
+
+export const useInvalidateAllUsers = () => {
+  const queryClient = useQueryClient();
+
+  const invalidate = useCallback(() => {
+    queryClient.invalidateQueries([getAllUsersKey]);
+  }, [queryClient]);
+
+  return {
+    invalidate,
+  };
 };
--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,14 +1,17 @@
 import { errorHandler } from "@/lib/error";
 import { useUserClient } from "@/state/client";
 import { useCallback } from "react";
+import { useInvalidateAllUsers } from "./useAllUsers";
 
 type Handler = (message: string) => void;
 export const useRegister = (handler: Handler) => {
   const { userClient } = useUserClient();
+  const { invalidate } = useInvalidateAllUsers();
   const register = useCallback(
     async (sessionToken: string, username: string, password: string) => {
       try {
         await userClient.register(sessionToken, { username, password });
+        invalidate();
       } catch (e) {
         errorHandler(e, handler);
       }
