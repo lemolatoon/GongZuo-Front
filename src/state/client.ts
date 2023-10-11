bd562212574c6554c2edfb11c265431d9662aa53
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-10-11 12:31:05 +0900

    Update config of client

--- src/state/client.ts
+++ src/state/client.ts
@@ -2,7 +2,7 @@ import { Configuration, GongzuosApi, UsersApi } from "@/apiClient";
 import { create } from "zustand";
 
 const config = new Configuration({
-  basePath: process.env.BASE_PATH ?? "http://localhost:3001",
+  basePath: process.env["NEXT_PUBLIC_BASE_PATH"] ?? "http://localhost:3001",
 });
 const gongzuoClient = new GongzuosApi(config);
 const userClient = new UsersApi(config);
